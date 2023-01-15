82541ac87d229d7467902a26934f0f397391b7c7
muahaio123 <longle9a@gmail.com>
2023-01-14 23:10:54 -0500

    update to resolve token to also return the data

--- my-app/src/profile/profile.tsx
+++ my-app/src/profile/profile.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import ProfileService, {Contact} from "../services/profile-service";
+import {useEffect, useState} from "react";
 
-export default function Profile(): JSX.Element{
+export default function Profile() {
+    let [contact1, setContact1] = useState<Contact>();
+    let [contact2, setContact2] = useState<Contact>();
+    let [contact3, setContact3] = useState<Contact>();
+
+    useEffect(() => {
+        ProfileService.getEmployeeContacts()
+            .then(r => {
+                console.log(r);
+                if (r.length > 0)
+                    setContact1((prevState) => r[0]);
+                if (r.length > 1)
+                    setContact2((prevState) => r[1]);
+                if (r.length > 2)
+                    setContact3((prevState) => r[2]);
+                console.log(contact1, contact2, contact3);
+            });
+    }, [])
+
+
+    let employeeEmail = localStorage.getItem("employeeEmail");
+
     return (
-        <h1>Profile here</h1>
-    )
+        <div>
+            <h1>Profile works!</h1>
+            <h2>Employee Email {employeeEmail}</h2>
+            <h3>{contact1?.address}</h3>
+            <h3>{contact2?.address}</h3>
+            <h3>{contact3?.address}</h3>
+            <button onClick={ProfileService.getEmployeeContacts}>getEmployeeContacts()</button>
+        </div>
+    );
+}
+
+function timeout(delay: number) {
+    return new Promise( res => setTimeout(res, delay) );
 }
